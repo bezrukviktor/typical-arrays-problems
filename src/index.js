4699f33d0fb5f98b102263afd0ac6ac01a58e80d
Viktor Bezruk <without.hands95@gmail.com>
2020-09-28 21:38:37 +0300

    feat: implement 3 functions: max, min and avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
+exports.min = function min(array) {
+    if (array === undefined || array.length === 0) {
+        return 0;
+    }
+    const reducer = (acc, curNum) => Math.min(acc, curNum);
+    return array.reduce(reducer);
+};
 
-exports.min = function min (array) {
-  return 0;
-}
 
-exports.max = function max (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (array === undefined || array.length === 0) {
+        return 0;
+    }
+    const reducer = (acc, curNum) => Math.max(acc, curNum);
+    return array.reduce(reducer);
+};
 
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (array === undefined || array.length === 0) {
+        return 0;
+    }
+    const reducer = (acc, curNum) => acc + curNum;
+    return array.reduce(reducer) / array.length;
+};
